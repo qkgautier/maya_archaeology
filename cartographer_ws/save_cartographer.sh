dfe80aee7e7c56fc73b6a8e621dae45c639517c1
Quentin Gautier <qkgautier@eng.ucsd.edu>
2017-06-02 19:01:34 -0700

    Updated cartographer script

--- cartographer_ws/save_cartographer.sh
+++ cartographer_ws/save_cartographer.sh
@@ -3,5 +3,6 @@
 DATE=`date +%Y_%m_%d_%H_%M_%S`
 
 source /home/e4e/cartographer_ws/install_isolated/setup.bash
-rosservice call /finish_trajectory "cartog_$DATE"
+rosservice call /finish_trajectory "trajectory_id: 0"
+rosservice call /write_assets "cartog_$DATE"
 
