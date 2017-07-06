3e50dc49483a0106318f037d0243f0c3460ce99e
Quentin Gautier <qkgautier@eng.ucsd.edu>
2017-07-06 14:50:41 -0700

    Updating scripts with modifications from the field

--- setup_vnc.sh
+++ setup_vnc.sh
@@ -3,6 +3,6 @@
 export DISPLAY=:0
 
 if [[ `ps -A | grep x11vnc | awk -f " " '{print $1}'` == "" ]];
-	        then
-				                x11vnc -nomodtweak -bg -reopen -forever -shared -display :0
-							fi
+then
+	x11vnc -nomodtweak -bg -reopen -forever -shared -display :0 -ncache 10
+fi
