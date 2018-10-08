968074870ad9a87697aa019f6b7dfd869991661a
Quentin Gautier <qkgautier@eng.ucsd.edu>
2018-10-08 11:47:58 -0700

    Updated RTABMap launch scripts

--- rtabmap/rtabmap_kinectv2.sh
+++ rtabmap/rtabmap_kinectv2.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-DATE=`date +%Y_%m_%d_%H_%M_%S`
+dblink="/home/e4e/.ros/rtabmap.db"
 
-ln -fs "/home/e4e/.ros/rtabmap_$DATE.db" "/home/e4e/.ros/rtabmap.db"
+DATE=`date +%Y_%m_%d_%H_%M_%S`
+dbfilename="/media/e4e/Data2/.ros/rtabmap_$DATE.db"
+
+if [ -f $dblink	]; then
+    rm $dblink
+fi
+
+ln -fs ${dbfilename} ${dblink}
 
 roslaunch rtabmap_kinectv2.launch "$@"
 
-rm ~/.ros/rtabmap.db
+rm $dblink
+
+echo "Database saved to $dbfilename"
 
