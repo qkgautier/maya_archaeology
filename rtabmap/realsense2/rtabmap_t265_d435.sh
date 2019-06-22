a6a66ee5b43be8c518ee531b864aa489134dd870
E4E MSI GS65 <e4e@eng.ucsd.edu>
2019-06-21 17:14:27 -0700

    Updated rtabmap d435/t265 scripts

--- rtabmap/realsense2/rtabmap_t265_d435.sh
+++ rtabmap/realsense2/rtabmap_t265_d435.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+dblink="/home/e4e/.ros/rtabmap.db"
+
 DATE=`date +%Y_%m_%d_%H_%M_%S`
 
-ln -fs "/home/e4e/.ros/rtabmap_$DATE.db" "/home/e4e/.ros/rtabmap.db"
+if [ -z "$ROS_HOME" ]; then
+	ROS_HOME=$HOME/.ros
+fi
+
+dbfilename="$ROS_HOME/rtabmap_$DATE.db"
+
+if [ -f $dblink	]; then
+    rm $dblink
+fi
+
+ln -fs ${dbfilename} ${dblink}
 
 roslaunch rtabmap_t265_d435.launch "$@"
 
-rm ~/.ros/rtabmap.db
+rm $dblink
+
+echo "Database saved to $dbfilename"
+
 
